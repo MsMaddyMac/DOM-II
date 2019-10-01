31479ef25052fbebf34c1ce230d9fe69a1e800eb
Madeline <mmcintosh2k14@gmail.com>
2019-10-01 15:40:51 -0400

    adds mouseenter event listener to nav header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const headerChange = document.querySelector("h1");
+headerChange.addEventListener("mouseenter", () => {
+    headerChange.textContent = "Has Arrived";
+    headerChange.style.color = "orange";
+    headerChange.style.transform = "scale(1.3";
+    headerChange.style.transition = "all 0.3s"
+})
+
