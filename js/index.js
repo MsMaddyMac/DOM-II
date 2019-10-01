e579c1317f5f5f41f5198b22510ba4f46922d8d1
Madeline <mmcintosh2k14@gmail.com>
2019-10-01 16:25:08 -0400

    adds event listeners to header sections

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 // Your code goes here
+
+// ------------ Header Section ------------ //
+
 const headerChange = document.querySelector("h1");
 headerChange.addEventListener("mouseenter", () => {
     headerChange.textContent = "Has Arrived";
@@ -6,4 +9,13 @@ headerChange.addEventListener("mouseenter", () => {
     headerChange.style.transform = "scale(1.3";
     headerChange.style.transition = "all 0.3s"
 })
+
+document.querySelectorAll(".nav-link").forEach(el => {
+    el.addEventListener("click", () => {
+    el.style.writingMode = "vertical-rl";
+    })
+})
+
+// ------------ Ends of Header Section ------------ //
+
 
