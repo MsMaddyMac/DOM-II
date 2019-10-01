debc5dcae254871658d6937e7c0987b2058af3fd
Madeline <mmcintosh2k14@gmail.com>
2019-10-01 17:34:05 -0400

    adds mouseleave eventListener to intro-img

--- js/index.js
+++ js/index.js
@@ -18,4 +18,23 @@ document.querySelectorAll(".nav-link").forEach(el => {
 
 // ------------ Ends of Header Section ------------ //
 
+const body = document.querySelector("body");
+window.addEventListener('scroll', () => {
+    body.style.backgroundImage = "linear-gradient(#f65b46, #f8e10f, #5adee1, #98d2b9)";
+})
 
+document.querySelectorAll("p").forEach(textColor => {
+    window.addEventListener("keypress", () => {
+    textColor.style.color = "#0e3cbf";
+    })
+})
+
+const busImage = document.querySelector(".intro img");
+busImage.addEventListener("mouseenter", () => {
+    busImage.style.transform = "scale(-0.7)";
+    busImage.style.transition = "all 0.3s"
+})
+busImage.addEventListener("mouseleave", () => {
+    busImage.style.transform = "scale(1)";
+    busImage.style.transition = "all 0.3s"
+})
